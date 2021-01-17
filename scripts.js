717f1b384e60eeef30ce98464a311af73a0f0b24
Madina Tadjidinova <madina.tadjidinova@gmail.com>
2021-01-17 21:39:00 +0600

    Added the formula "Formula of variable"

--- scripts.js
+++ scripts.js
@@ -3,3 +3,11 @@ let first = +prompt("Длина прямоугольника");
 let second = +prompt("Ширина прямоуголььника");
 let result = first * second;
 console.log(`S = ${result}`);
+
+//Формула переменного 
+let a = +prompt("Значение превого А");
+let a1 = +prompt("Значение второго А");
+let b = +prompt("Значение превого В");
+let b1 = +prompt("Значение превого В");
+let sum = a + a1 + b + b1;
+console.log(`P = ${sum}`);
